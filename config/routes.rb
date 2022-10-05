7f15cf31cf3330c5f1cf9ad963bda83d9b069860
Ubuntu <ubuntu@ip-172-31-1-219.ec2.internal>
2022-10-05 09:46:31 +0000

    L41

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+    get 'top/main'
+    root 'top#main'
+    post 'top/login'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
